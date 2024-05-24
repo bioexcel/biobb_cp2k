af66b4bcdf01333f544344d858899992e5207fd2
gbayarri <genis.bayarri@irbbarcelona.org>
2024-05-24 10:57:41 +0200

    2024.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_cp2k",
-    version="4.1.0",
+    version="4.2.0",
     author="Biobb developers",
     author_email="adam.hospital@irbbarcelona.org",
     description="Biobb_cp2k is a BioBB category for CP2K QM package.",
@@ -19,7 +19,7 @@ setuptools.setup(
     },
     packages=setuptools.find_packages(exclude=['docs', 'test']),
     include_package_data=True,
-    install_requires=['biobb_common==4.1.0'],
+    install_requires=['biobb_common==4.2.0'],
     python_requires='>=3.8',
     entry_points={
         "console_scripts": [
